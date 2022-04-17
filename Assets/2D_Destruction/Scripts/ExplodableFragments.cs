9810093930c905d475b3704f480f838ccdd20977
zclucas <2660681757@qq.com>
2022-04-17 14:02:32 +0800

    my modify

---
+++ Assets/2D_Destruction/Scripts/ExplodableFragments.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class ExplodableFragments : ExplodableAddon{
+    public override void OnFragmentsGenerated(List<GameObject> fragments)
+    {
+        foreach (GameObject fragment in fragments)
+        {
+            Explodable fragExp = fragment.AddComponent<Explodable>();
+			fragment.AddComponent<ExplodeOnClick>();
+
+            fragExp.shatterType = explodable.shatterType;
+            fragExp.fragmentLayer = explodable.fragmentLayer;
+            fragExp.sortingLayerName = explodable.sortingLayerName;
+            fragExp.orderInLayer = explodable.orderInLayer;
+            fragExp.transform.parent = explodable.Parent;
+            fragExp.Parent = explodable.Parent;
+            fragment.layer = explodable.gameObject.layer;
+
+            fragExp.fragmentInEditor();
+        }
+    }
+}
---
+++ Assets/2D_Destruction/Scripts/ExplodeOnClick.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Explodable))]
+public class ExplodeOnClick : MonoBehaviour {
+
+	private Explodable _explodable;
+
+	void Start()
+	{
+		_explodable = GetComponent<Explodable>();
+	}
+	void OnMouseDown()
+	{
+		_explodable.explode();
+		ExplosionForce ef = FindObjectOfType<ExplosionForce>();
+		float oriZ = Camera.main.WorldToScreenPoint(transform.position).z;
+		Vector3 tar = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, oriZ));
+		ef.doExplosion(tar);
+	}
+}
